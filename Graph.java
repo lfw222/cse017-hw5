322b6ef6868ee5313a67c04f50a7995766060d85
Lena Weisman <lenaweismanhome@Lenas-MBP.lpcnextlight.net>
2020-08-05 17:55:17 -0600

    graph progress

--- Graph.java
+++ Graph.java
@@ -6,6 +6,22 @@ public class Graph {
     }
     public void add_edge(int from, int to){
         graph[from][to] = 1;
+        graph[to][from]=1; // need to make graph undirected
+    }
+    public void remove_egde(int from, int to){
+        graph[from][to] = 0;
+        graph[to][from]= 0;
+    }
+    public int [] traverse_bfs(int start){
+        Queue Q = new Queue(5);
+        boolean [] visited = new boolean [graph.length];
+        push(start);
+        while(Q.this.size > 0){
+            int n = Q.pop();
+            if(n == 1){
+
+            }
+        }
     }
     public void print(){
         for(int i=0; i < graph.length; i++){
@@ -30,6 +46,10 @@ public class Graph {
         G.print();
 
         G.add_edge(0,4);
+        G.print();
+
+        G.remove_egde(0, 4);
+
         G.print();
 
 
