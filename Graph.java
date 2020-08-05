7fb8d828a71f48788aa027d5bf50747a3173496e
Lena Weisman <lenaweismanhome@Lenas-MBP.lpcnextlight.net>
2020-08-05 17:04:45 -0600

    graph start

--- Graph.java
+++ Graph.java
@@ -1,3 +1,38 @@
 public class Graph {
+    private int [][] graph;
     
+    public Graph(int [][] graph){
+        this.graph = graph;
+    }
+    public void add_edge(int from, int to){
+        graph[from][to] = 1;
+    }
+    public void print(){
+        for(int i=0; i < graph.length; i++){
+            for(int j=0; j< graph[i].length; j++){
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main (String [] args){
+        
+        int [][] adjMatrix = {
+            {0,1,1,0,0},
+            {1,0,1,1,0},
+            {1,1,0,0,1},
+            {0,1,0,0,1},
+            {0,0,1,1,0},
+        };
+        Graph G = new Graph(adjMatrix);
+        G.print();
+
+        G.add_edge(0,4);
+        G.print();
+
+
+
+    }
 }
